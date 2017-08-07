e5e5fc60b47f29b3de44a46325b6f51ba7d6dae9
nibumjohn <30754559+nibumjohn@users.noreply.github.com>
2017-08-07 20:45:08 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articleName', function (req, res){
+app.get('/:articleName', function (req, res){
     // artilceName == article-one
     //article[articleName] == {} content object for article one
     var articleName = req.params.articleName;
