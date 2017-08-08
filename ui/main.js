11395e4940da73be3db4b331e1aaa1c1b6987db4
nibumjohn <30754559+nibumjohn@users.noreply.github.com>
2017-08-08 23:43:32 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,4 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-
-element.innerHtml = 'New value';
-
-//move the image
-var img = document.getElementById('mandi');
-var marginLeft= 0;
-    function moveRight () {
-    marginLeft = marginLeft+1;
-    ing.style.marginLeft = marginLeft + 'px';
-    }
-    ing.onclick = function () {
-    var interval = setInterval (moveRight, 50);
-    };
-    
+  
 //counter code
 var button = document.getElementById('counter');
 var counter = 0;
